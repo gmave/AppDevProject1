5800ccd7ad5d9e3c9b9e3cc9bb9e7491923e4e98
gmave <seventhstarlight0@gmail.com>
2024-09-01 18:03:24 +0800

    Added personalized greetings.

--- PracticeTest/src/App.java
+++ PracticeTest/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Farewell, Rock!");
+        try (Scanner scanUser = new Scanner(System.in)) {
+        System.out.println("Enter your name: ");
+        String userName = scanUser.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        }
     }
 }
