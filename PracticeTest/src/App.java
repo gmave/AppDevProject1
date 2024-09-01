5b18673f0579947ceefa4596d38a48078c580aee
gmave <seventhstarlight0@gmail.com>
2024-09-01 18:42:28 +0800

    Add personalized goodbye greeting feature

--- PracticeTest/src/App.java
+++ PracticeTest/src/App.java
@@ -7,6 +7,7 @@ public class App {
         System.out.println("Enter your name: ");
         String userName = scanUser.nextLine();
         System.out.println("Hello, " + userName + "!");
+        System.out.println("And goodbye" + userName + "!");
         }
     }
 }
